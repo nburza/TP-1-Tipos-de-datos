6f828378ed9e237aa39e716ef6fd1d7f7b4d72df
nburza <nicoburza_96@hotmail.com>
2015-04-16 00:37:26 -0300

    tipos de datos

---
+++ tipos de datos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+
+int num = 3;
+int num2 = 4;
+bool suma = num + num2 == 7;
+char letra = 'n';
+char* cadena = "icolas";
+double num3 = 70872057.68;
+double num4 = 657335.62;
+double resta = num3 - num4;
+
+using namespace std;
+
+int main()
+{
+	cout << suma << "\n";
+	cout << resta << "\n";
+	cout << letra << cadena << "\n" << endl;
+}
